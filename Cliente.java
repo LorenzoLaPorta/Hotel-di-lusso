4b0d304612a463d55f6afa12d7d5448aed71378b
Lorenzo La Porta <s.lorenzo.laporta@polofermigiorgi.it>
2024-11-13 13:19:21 +0100

    v3
    ho migliorato i vari commenti di metodi e funzioni per essere più chiari, precisi e funzionali

--- Cliente.java
+++ Cliente.java
@@ -1,25 +1,40 @@
 import java.util.Scanner;
 
+
 public class Cliente
 {
+
 
     //creo gli attributi necessari per ogni Cliente
     String nome;
     int giornoIngresso;
    
-    //creo il costruttore
+    /**
+     * creo il costruttore
+     * 
+     * @param gli attributi nome e giornoIngresso
+     */
     Cliente(String nome, int giornoIngresso){
         this.nome = nome;
         this.giornoIngresso = giornoIngresso;
     }
    
-    //faccio l'override del metodo toString per fargli stampare nome del Cliente e giorno di ingresso
+    /**
+     * faccio l'override del metodo toString per fargli stampare nome del Cliente e giorno di ingresso
+     * 
+     * @return il nome e giorno d'ingresso del cliente
+     */
     @Override
     public String toString(){
         return "nome del Cliente: " + this.nome + ", giorno di ingresso: " + this.giornoIngresso;
     }
 
-    //creo il metodo trovaIndiceVuoto, che restituisce il primo indice senza oggetti dell'array camereSingole
+    /**
+     * creo il metodo trovaIndiceVuoto, che restituisce il primo indice senza oggetti dell'array camereSingole
+     * 
+     * @param l'array camereSingole
+     * @return l'indice della camera vuota (-1 se non esistono camere vuote)
+     */
     public static int trovaIndiceVuoto(Cliente[] camereSingole){
         for (int i = 0; i < camereSingole.length; i++){
             if (camereSingole[i] == null){
@@ -29,7 +44,12 @@ public class Cliente
         return -1;
     }
 
-    //creo il metodo checkIn, che permette l'inserimento di nuovi clienti nelle stanze e restituisce false se l'array e' pieno o true se non e' pieno
+    /**
+     * creo il metodo checkIn, che permette l'inserimento di nuovi clienti nelle stanze e restituisce false se l'array e' pieno o true se non e' pieno
+     * 
+     * @param l'array camereSingole e l'oggetto c1
+     * @return l'indice della camera che viene riempita (-1 se non viene riempita nessuna camera)
+     */
     public static boolean checkIn(Cliente[] camereSingole, Cliente c1){
         int indiceVuoto = trovaIndiceVuoto(camereSingole);
         if (indiceVuoto == -1){
@@ -41,7 +61,12 @@ public class Cliente
         }
     }
 
-    //creo il metodo ricerca Cliente che cerca e restituisce l'indice di un Cliente inserito
+    /**
+     * creo il metodo ricerca Cliente che cerca e restituisce l'indice di un Cliente inserito
+     * 
+     * @param l'array camereSingole e la variabile ClienteCheckout
+     * @return l'indice del cliente cercato (-1 se non esiste)
+     */
     public static int riecrcaCliente(Cliente[] camereSingole, String ClienteCheckout){
         for (int i = 0; i < camereSingole.length; i++){
             if (camereSingole[i] != null){
@@ -53,7 +78,12 @@ public class Cliente
         return -1;
     }
 
-    //creo il metodo pagamento che calcola il prezzo che deve pagare il Cliente inserito
+    /**
+     * creo il metodo pagamento che calcola il prezzo che deve pagare il Cliente inserito
+     * 
+     * @param l'array camereSingole, la variabile ClienteCheckout, la variabile giornoAttuale e la variabile prezzoANotte
+     * @return il prezzo che deve pagare il cliente (-1 se non viene trovato dalla funzione ricercaCliente)
+     */
     public static int pagamento(Cliente[] camereSingole, String ClienteCheckout, int giornoAttuale, int prezzoANotte){
         int clienteRicercato = riecrcaCliente(camereSingole, ClienteCheckout);
         if (clienteRicercato != -1){
@@ -62,7 +92,11 @@ public class Cliente
         return -1;
     }
 
-    //creo il metodo checkOut che elimina il Cliente inserito dall'array camereSingole
+    /**
+     * creo il metodo checkOut che elimina il Cliente inserito dall'array camereSingole
+     * 
+     * @param l'array camereSingole e la variabile ClienteCheckout
+     */
     public static void checkOut(Cliente[] camereSingole, String ClienteCheckout){
         int clienteRicercato = riecrcaCliente(camereSingole, ClienteCheckout);
         if (clienteRicercato != -1){
@@ -70,7 +104,11 @@ public class Cliente
         }
     }
 
-    //creo il metodo checkOut che stampa tutte le camere libere
+    /**
+     * creo il metodo stampaCamereLibere, che stampa tutte le camere libere
+     * 
+     * @param l'array camereSingole
+     */
     public static void stampaCamereLibere(Cliente[] camereSingole){
         System.out.println("CAMERE LIBERE:");
         for (int i = 0; i < camereSingole.length; i++){
@@ -80,7 +118,11 @@ public class Cliente
         }
     }
 
-    //creo il metodo checkOut che stampa tutte le camere occupate
+    /**
+     * creo il metodo stampaCamereOccupate, che stampa tutte le camere occupate
+     * 
+     * @param l'array camereSingole
+     */
     public static void stampaCamereOccupate(Cliente[] camereSingole){
         System.out.println("CAMERE OCCUPATE:");
         for (int i = 0; i < camereSingole.length; i++){
@@ -90,21 +132,27 @@ public class Cliente
         }
     }
 
+
     public static void main(String[] args) {  
+
 
         Scanner scanner = new Scanner(System.in);
 
+
         final int N = 5; //creo la costante
+
 
         //creo i due array rappresentanti le camere
         Cliente[] camereSingole = new Cliente [N];
         //Cliente[] cameredoppie = new Cliente [N]
+
 
         String selezioneMenu;//creo la variabile "selezioneMenu" per selezionare una funzione
         String ClienteCheckout;//creo la variabile "ClienteCheckout" per inserire il Cliente che deve uscire e pagare
         int giornoAttuale;//creo la variabile "giornoAttuale" per definire il numero di giorno attuale
         int prezzoANotte = 300;//creo la variabile "prezzoAnotte" per definire il prezzo a notte di una camera dell'hotel
         int prezzo;//creo la variabile "prezzo", a cui verra' assegnato il prezzo finale di checkout
+
 
         do{
             System.out.println("Cosa vuoi fare? Seleziona un numero per continuare\n0) esci dal programma\n1) Check-in\n2) Check-out\n3) stampa camere libere\n4) stampa camere occupate\n");
@@ -122,7 +170,7 @@ public class Cliente
                         System.out.println("Le camere sono tutte occupate! Il Cliente non puo' entrare");
                     }
                     break;
-                case "2": 
+                case "2":
                     System.out.println("Inserisci il nome del cliente che deve fare checkout");
                     ClienteCheckout = scanner.nextLine();
                     System.out.println("Inserisci il giorno del mese attuale in formato numerico");
